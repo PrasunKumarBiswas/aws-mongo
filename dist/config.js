52082e5ac461dcf7c93b20313684a5f53492ea97
PrasunKumarBiswas <prasun.kumar@yahoo.com>
2022-12-18 20:25:47 +0530

    commit

--- dist/config.js
+++ dist/config.js
@@ -8,6 +8,7 @@ var _dotenv = _interopRequireDefault(require("dotenv"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 _dotenv.default.config();
 var _default = {
+  PORT: process.env.PORT || 5000,
   MONGODB_URL: process.env.MONGODB_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID
